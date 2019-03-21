58f50c74599e30ef19afabcee52ca7fefa545e9d
Shalabi <m_shalabi2@tss-hr420-dm33.fc.ca>
2019-03-21 16:44:41 -0400

    new commiots

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    user: "root",
+    password: "root", // blank on windows
+    port: 8889, // for windows 3306
+    host: "localhost",
+    database: "marvelcharacters"
+
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // get data from the db, and then send it through the rout
+  // to the index.hbs page and then render that (including our db data)
+  connect.query(`SELECT name, avatar FROM hero`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('index', { avatars: result });
+    }
+  });
+  //res.render('index', { title: 'Express' });
 });
+
+
+
+/* GET home page. */
+router.get('/:hero', function(req, res, next) {
+  // get data from the db, and then send it through the rout
+  // to the index.hbs page and then render that (including our db data)
+  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`, (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
+      res.render('bio', { bioData: result[0]});
+    }
+  });
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
 
 module.exports = router;
